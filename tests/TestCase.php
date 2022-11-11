07a92a62090eefc3f8efb7db6c5a627ad4f7fbf3
Lucas Berto <lucaum.berto@gmail.com>
2022-11-11 15:32:32 -0300

    .

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,8 +20,5 @@ class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        $app['config']->set('test_bling_token', '7d6265da915c702875a58634656af0cfe4cffabf4c2ad58637c174475f976920fd59c414');
-        $app['config']->set('test_store_code', '204153875');
-        $app['config']->set('test_sku', '807201');
     }
 }
